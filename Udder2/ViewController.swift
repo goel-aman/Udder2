0595a805488c8407f56ad490a3b46f06e4466eba
aman goel <amangoel9873572693@gmail.com>
2022-02-23 10:37:37 +0530

    Initial Commit

---
+++ Udder2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder2
+//
+//  Created by Aman Goel on 23/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
